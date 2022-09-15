01b8e522723288d20e840d5852e7c45121a8ad50
William <williamsantana5750@gmail.com>
2022-09-15 11:07:24 -0300

    Efetuando a impressão dos card's no front-end

--- public/script.js
+++ public/script.js
@@ -8,8 +8,33 @@ function update() {
       return element.json()
     })
     .then(element => {
-      element.forEach(a => {
-        console.log(a.title, a.description)
+      let posts = ' '
+      element.forEach(post => {
+        posts += `<div class="card mb-2 text-center">
+                     <div class="card-body">
+                       <h5 class="card-title">${post.title}</h5>
+                       <p class="card-text">${post.description}</p>
+                    </div>
+                 <div class="card-footer text-muted">Atual: ${post.date}</div>
+               </div>`
       })
+      document.getElementById('cards').innerHTML += posts
     })
+}
+
+function newPost() {
+  let title = document.getElementById('inputTitle').value
+  let description = document.getElementById('inputDescription').value
+
+  let post = { title, description }
+
+  let options = {
+    method: 'POST',
+    headers: new Headers({ 'content-type': 'application/json' }),
+    body: JSON.stringify(post)
+  }
+
+  fetch('http://localhost:3333/new', options).then(res => {
+    update()
+  })
 }
